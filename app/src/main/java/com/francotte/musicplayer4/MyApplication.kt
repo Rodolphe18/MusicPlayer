21e4bde8782dcb8d8aa2242aab407bd1f3f4e61c
Rodolphe18 <rodolphefrancotte18@gmail.com>
2022-11-13 14:01:45 +0100

    Add dagger hilt modules (Glide + Services)

---
+++ app/src/main/java/com/francotte/musicplayer4/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.francotte.musicplayer4
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+}
---
+++ app/src/main/java/com/francotte/musicplayer4/di/AppModule.kt
@@ -0,0 +1,29 @@
+package com.francotte.musicplayer4.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.francotte.musicplayer4.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideGlideInstance(
+       @ApplicationContext context : Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.image_cd)
+            .error(R.drawable.image_cd)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
+}
