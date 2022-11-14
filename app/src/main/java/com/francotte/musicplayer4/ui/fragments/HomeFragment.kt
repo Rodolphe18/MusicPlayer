2ddef77b6ac7949dfbbd745978b8831e7eec484c
Rodolphe18 <rodolphefrancotte18@gmail.com>
2022-11-14 15:42:48 +0100

    Song RecyclerView Created

---
+++ app/src/main/java/com/francotte/musicplayer4/ui/fragments/HomeFragment.kt
@@ -0,0 +1,52 @@
+package com.francotte.musicplayer4.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.francotte.musicplayer4.R
+import com.francotte.musicplayer4.adapters.SongAdapter
+import com.francotte.musicplayer4.other.Status
+import com.francotte.musicplayer4.ui.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    lateinit var mainViewModel: MainViewModel
+
+    @Inject
+    lateinit var songAdapter : SongAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+
+        songAdapter.setOnItemClickListener {
+            mainViewModel.playOrToggleSong(it)
+        }
+    }
+
+    private fun setupRecyclerView() = rvAllSongs.apply {
+        adapter = songAdapter
+        layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun subscribeToObservers() {
+        mainViewModel.mediaItems.observe(viewLifecycleOwner) { result ->
+            when(result.status) {
+                Status.SUCCESS -> {
+                    allSongsProgressBar.isVisible = true
+                    result.data?.let { songs ->
+                        songAdapter.songs = songs
+                    }
+                }
+                Status.ERROR -> Unit
+                Status.LOADING -> allSongsProgressBar.isVisible = true
+            }
+        }
+    }
+}
